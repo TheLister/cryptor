13e962a77f6fb0cfa962523433a51c401b06e4f5
David Lister <whizkid2012@gmail.com>
2020-10-14 12:09:27 -0500

    asdasd

--- encrypting.py
+++ encrypting.py
@@ -5,11 +5,14 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 
-password_provided = "password" 
-print(password_provided)
+
+# gather inputs
+password_provided = input("Enter your symmetric key: ") 
+message = input("Enter your message: ")
+
+print('Using key "' + password_provided + '"' + ' to encrypt "' + message + '"')
 password = password_provided.encode() #convert to type bytes
 salt = b'p\xcckM\x15\xeb\xa6\xf9K\xe2l\xe7\xc49\xfe\xee'
-print(salt)
 
 kdf = PBKDF2HMAC( 
     algorithm=hashes.SHA256(),
@@ -20,18 +23,16 @@ kdf = PBKDF2HMAC(
 )
 
 key = base64.urlsafe_b64encode(kdf.derive(password))
-print("key")
 print(key)
 
-file = open('key.key', 'wb')
-file.write(key) #key is type bytes
-file.close()
+# file = open('key.key', 'wb')
+# file.write(key) #key is type bytes
+# file.close()
 
-message = "my secret message"
-print(message)
+
 encoded_message = message.encode()
 
 fernet_object = Fernet(key)
 encrypted_message = fernet_object.encrypt(encoded_message)
-print("encrypted message:")
-print(encrypted_message)
+print("the next line contains your encrypted message:")
+print(encrypted_message)    
