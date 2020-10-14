b187402bcbe2ee0a2266f82b994f47c05258c8fa
David Lister <whizkid2012@gmail.com>
2020-10-14 11:48:33 -0500

    asd

--- encrypting.py
+++ encrypting.py
@@ -1,8 +1,37 @@
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 
-key = Fernet.generate_key()
+password_provided = "password" 
+print(password_provided)
+password = password_provided.encode() #convert to type bytes
+salt = b'p\xcckM\x15\xeb\xa6\xf9K\xe2l\xe7\xc49\xfe\xee'
+print(salt)
+
+kdf = PBKDF2HMAC( 
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
+
+key = base64.urlsafe_b64encode(kdf.derive(password))
+print("key")
 print(key)
 
 file = open('key.key', 'wb')
 file.write(key) #key is type bytes
 file.close()
+
+message = "my secret message"
+print(message)
+encoded_message = message.encode()
+
+fernet_object = Fernet(key)
+encrypted_message = fernet_object.encrypt(encoded_message)
+print("encrypted message:")
+print(encrypted_message)
