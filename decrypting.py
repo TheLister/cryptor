a067ca5e95712375ef6dc34890916f88cf61bebb
David Lister <whizkid2012@gmail.com>
2020-10-14 12:17:07 -0500

    today

--- decrypting.py
+++ decrypting.py
@@ -5,7 +5,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 
-password_provided = "password" 
+password_provided = input("password" 
 print(password_provided)
 password = password_provided.encode() #convert to type bytes
 salt = b'p\xcckM\x15\xeb\xa6\xf9K\xe2l\xe7\xc49\xfe\xee'
@@ -20,7 +20,6 @@ kdf = PBKDF2HMAC(
 )
 
 key = base64.urlsafe_b64encode(kdf.derive(password))
-print("key")
 print(key)
 
 
--- encrypting.py
+++ encrypting.py
@@ -7,10 +7,10 @@ from cryptography.fernet import Fernet
 
 
 # gather inputs
-password_provided = input("Enter your symmetric key: ") 
+password_provided = input("Create a password: ") 
 message = input("Enter your message: ")
 
-print('Using key "' + password_provided + '"' + ' to encrypt "' + message + '"')
+
 password = password_provided.encode() #convert to type bytes
 salt = b'p\xcckM\x15\xeb\xa6\xf9K\xe2l\xe7\xc49\xfe\xee'
 
@@ -23,16 +23,22 @@ kdf = PBKDF2HMAC(
 )
 
 key = base64.urlsafe_b64encode(kdf.derive(password))
-print(key)
 
-# file = open('key.key', 'wb')
-# file.write(key) #key is type bytes
-# file.close()
+
+file = open('key.key', 'wb')
+file.write(key) #key is typebytes
+file.close()
 
 
 encoded_message = message.encode()
 
 fernet_object = Fernet(key)
 encrypted_message = fernet_object.encrypt(encoded_message)
+
+print('Using password "' + password_provided + '"' + ' to encrypt "' + message + '"')
+
+print("the next line contains your key:")
+print(key)
+
 print("the next line contains your encrypted message:")
 print(encrypted_message)    
