b9b3fbccd5b62b378e9ee99eaef5ad11a464ea4b
TheLister <whizkid2012@gmail.com>
2020-10-14 22:37:21 -0500

    absolute garbage but works

--- decrypting.py
+++ decrypting.py
@@ -5,29 +5,36 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 
-password_provided = input("password" 
-print(password_provided)
-password = password_provided.encode() #convert to type bytes
-salt = b'p\xcckM\x15\xeb\xa6\xf9K\xe2l\xe7\xc49\xfe\xee'
-print(salt)
-
-kdf = PBKDF2HMAC( 
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt,
-    iterations=100000,
-    backend=default_backend()
-)
-
-key = base64.urlsafe_b64encode(kdf.derive(password))
-print(key)
 
 
-encrypted_message = "my secret message"
-print(message)
-encoded_message = message.encode()
+# Gather Inputs
+encrypted_message_provided = input("Paste your encrypted message: ")
+key_provided = input("Paste your key: ")
 
-fernet_object = Fernet(key)
-encrypted_message = fernet_object.encrypt(encoded_message)
-print("encrypted message:")
-print(encrypted_message)
+
+
+# password = password_provided.encode() #convert to type bytes
+# salt = b'p\xcckM\x15\xeb\xa6\xf9K\xe2l\xe7\xc49\xfe\xee'
+# print(salt)
+
+# kdf = PBKDF2HMAC( 
+#     algorithm=hashes.SHA256(),
+#     length=32,
+#     salt=salt,
+#     iterations=100000,
+#     backend=default_backend()
+# )
+
+# key = base64.urlsafe_b64encode(kdf.derive(password))
+# print(key)
+
+
+# encrypted_message = "my secret message"
+# print(message)
+# encoded_message = message.encode()
+
+fernet_token = Fernet(b'tM-cQqEaCssb3CObjR5cyTnmuqB07obBpYP8okiVu3I=')
+decrypted_message = fernet_token.decrypt(b'gAAAAABfh8NEeOYClBM5PxndMCCHIXo7EvRUn3g3xMv545Y0Y6QvfXEBD6fJfGqDYV05nJk9UFc-_2tdeIwTjnoT9erW4fm5Jw==')
+
+print("decrypted message:")
+print(decrypted_message)
